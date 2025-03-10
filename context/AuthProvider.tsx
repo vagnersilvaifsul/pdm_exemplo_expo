6e1bf8cf6cb1ceb1d128f6bbcae1a88e6b67fa25
vagnersilvaifsul <vagnersilva@ifsul.edu.br>
2025-03-10 09:25:09 -0300

    feat: entrar, retorno da mensagem de erro de auth

--- context/AuthProvider.tsx
+++ context/AuthProvider.tsx
@@ -22,9 +22,30 @@ export const AuthProvider = ({ children }: any) => {
 			return "ok";
 		} catch (error: any) {
 			console.error(error.code, error.message);
-			return "error";
+			return launchServerMessageErro(error);
 		}
 	}
+
+	//função utilitária
+	function launchServerMessageErro(e: any): string {
+		switch (e.code) {
+			case "auth/invalid-credential":
+				return "Email inexistente ou senha errada.";
+			case "auth/user-not-found":
+				return "Usuário não cadastrado.";
+			case "auth/wrong-password":
+				return "Erro na senha.";
+			case "auth/invalid-email":
+				return "Email inexistente.";
+			case "auth/user-disabled":
+				return "Usuário desabilitado.";
+			case "auth/email-already-in-use":
+				return "Email em uso. Tente outro email.";
+			default:
+				return "Erro desconhecido. Contate o administrador";
+		}
+	}
+
 	return (
 		<AuthContext.Provider value={{ signIn }}>{children}</AuthContext.Provider>
 	);

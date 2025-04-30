4395594c46927840b5f5ea5fdea5f82b5577e2e8
vagnersilvaifsul <vagnersilva@ifsul.edu.br>
2025-04-30 13:36:38 -0300

    feat: notificações, versão inicial para validação do método Expo para conectar o serviço FCM

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,124 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { AuthContext } from "@/context/AuthProvider";
+import * as Device from "expo-device";
+import * as Notifications from "expo-notifications";
 import { router } from "expo-router";
-import { useContext, useEffect } from "react";
-import { Image, StyleSheet } from "react-native";
+import { useContext, useEffect, useRef, useState } from "react";
+import { Image, Platform, StyleSheet } from "react-native";
 import { useTheme } from "react-native-paper";
 import { SafeAreaView } from "react-native-safe-area-context";
+
+//1. Configurar o manipulador de notificações
+Notifications.setNotificationHandler({
+	handleNotification: async () => ({
+		shouldShowAlert: true,
+		shouldPlaySound: true,
+		shouldSetBadge: false,
+	}),
+});
+
+/*
+	Outros passos devem ser dados, são eles:
+	6. Configuração do Firebase no projeto (envolve obter o google-services.json e configurar o app.json)
+	7. Criar o app em modo desenvolvimento (profile development), seguindo os passos de: https://expo.dev/blog/expo-go-vs-development-builds
+*/
 
 export default function PreloadScreen() {
 	const theme = useTheme();
 	const { recuperaCredencialdaCache, signIn } = useContext<any>(AuthContext);
+	const [expoPushToken, setExpoPushToken] = useState("");
+	const [notification, setNotification] = useState<
+		Notifications.Notification | undefined
+	>(undefined);
+	const notificationListener = useRef<Notifications.EventSubscription>();
+	const responseListener = useRef<Notifications.EventSubscription>();
 
 	useEffect(() => {
 		//ao montar o componente tenta logar com as credenciais da cache
 		logar();
+
+		registerForPushNotificationsAsync()
+			.then((token) => setExpoPushToken(token ?? ""))
+			.catch((error: any) => setExpoPushToken(`${error}`));
+
+		//4. Gerenciar notificações recebidas
+		notificationListener.current =
+			Notifications.addNotificationReceivedListener((notification) => {
+				setNotification(notification);
+				console.log(notification);
+			});
+
+		//5. Responda aos toques de notificação
+		responseListener.current =
+			Notifications.addNotificationResponseReceivedListener((response) => {
+				console.log(response);
+			});
+
+		return () => {
+			notificationListener.current &&
+				Notifications.removeNotificationSubscription(
+					notificationListener.current
+				);
+			responseListener.current &&
+				Notifications.removeNotificationSubscription(responseListener.current);
+		};
 	}, []);
+
+	async function registerForPushNotificationsAsync() {
+		if (Platform.OS === "android") {
+			//Configura o canal de notificação para Android
+			Notifications.setNotificationChannelAsync("default", {
+				name: "default",
+				importance: Notifications.AndroidImportance.MAX,
+				vibrationPattern: [0, 250, 250, 250],
+				lightColor: "#FF231F7C",
+			});
+		}
+
+		//2. Solicitar permissões de notificação
+		if (Device.isDevice) {
+			const { status: existingStatus } =
+				await Notifications.getPermissionsAsync();
+			let finalStatus = existingStatus;
+			if (existingStatus !== "granted") {
+				const { status } = await Notifications.requestPermissionsAsync();
+				finalStatus = status;
+			}
+			if (finalStatus !== "granted") {
+				handleRegistrationError(
+					"Permissão não concedida para obter o token para push notification!"
+				);
+				return;
+			}
+			const projectId =
+				Constants?.expoConfig?.extra?.eas?.projectId ??
+				Constants?.easConfig?.projectId;
+			if (!projectId) {
+				handleRegistrationError("Project ID not found");
+			}
+			//3. Obtenha o token Expo Push
+			try {
+				const pushTokenString = (
+					await Notifications.getExpoPushTokenAsync({
+						projectId,
+					})
+				).data;
+				console.log(pushTokenString);
+				return pushTokenString;
+			} catch (e: unknown) {
+				handleRegistrationError(`${e}`);
+			}
+		} else {
+			handleRegistrationError(
+				"Must use physical device for push notifications"
+			);
+		}
+	}
+
+	function handleRegistrationError(errorMessage: string) {
+		alert(errorMessage);
+		throw new Error(errorMessage);
+	}
 
 	async function logar() {
 		const credencial = await recuperaCredencialdaCache();

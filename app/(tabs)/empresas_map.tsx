5145dfeaa76235e2dad96c24e813b47b1daf7661
vagnersilvaifsul <vagnersilva@ifsul.edu.br>
2025-07-03 14:00:58 -0300

    docs: referência para o serviço do Google Maps

--- app/(tabs)/empresas_map.tsx
+++ app/(tabs)/empresas_map.tsx
@@ -7,7 +7,7 @@ import { Button, Icon, useTheme } from "react-native-paper";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 /*
-    1. Para que funcione o mapa é necessário instalar o react-native-maps e ativar o Google Maps SDK no console do GCP.
+    1. Para que funcione o mapa é necessário instalar o react-native-maps e ativar o Maps SDK for Android no console do GCP.
     Para abrir o console do GCP para o seu projeto, vá no Cosole do Firebase > Clique na engrenagem ao lado de "Visão Geral do Projeto"
     > Vá na Guia "Constas e serviço" > Vá em "Todas as contas de serviço" e clique no link com o ícone do GCP.
     No Dashboard do GCP confira se o nome do projeto é o mesmo none que você criou no Firebase (no campo de input, acima à esquerda).
